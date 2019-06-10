af915d9f23cee3118785666875c045bcc197d603
Charlotte <bgzclxqq@163.com>
2019-06-10 19:40:32 +0800

    完成蹭课界面，解决了freeroom的“重加载”
    解决方法：初始data不设数据，在onLoad时setData

--- client/pages/auditing/auditing.js
+++ client/pages/auditing/auditing.js
@@ -1,25 +1,206 @@
-// pages/auditing/auditing.js
-Page({
 
-  /**
-   * 页面的初始数据
-   */
+const app = getApp();
+
+Page({
   data: {
+    toptab: ['搜课程', '搜专业', '搜老师'],
+    currentTab: 0,
+    results: [],
+    reshow: 1,
+    empty: 1,
 
   },
+  // 提交课程名搜索
+  course_formSubmit: function (e) {
+    let that = this;
+    let formData = e.detail.value.keyword;// 关键词
+    console.log(formData);
 
+    if (formData == '') {
+      wx.showToast({
+        title: '输入不能为空',
+        icon: 'none'
+      })
+    } else {
+      wx.showLoading({
+        title: '搜索中...',
+        icon: 'Loading'
+      });
+
+      wx.request({
+        url: 'http://www.triple2.xyz:8081/schedule/cname',
+        data: {
+          cname: formData
+        },
+        success: function (res) {
+          console.log(res.data);
+          if (res.data.length == 0) {
+            console.log("无结果")
+            that.setData({
+              reshow: 2
+            })
+          } else {
+            console.log("有结果")
+            that.getcourseTime(res.data)
+            that.setData({
+              reshow: 1
+            })
+            console.log("reshow:")
+            console.log(that.data.reshow)
+          }
+          wx.hideLoading();
+        }
+      })
+    }
+  },
+  // 按专业搜索
+  major_formSubmit: function(e) {
+    let that = this;
+    let formData = e.detail.value.keyword;// 关键词
+    console.log(formData);
+
+    if (formData == '') {
+      wx.showToast({
+        title: '输入不能为空',
+        icon: 'none'
+      })
+    } else {
+      wx.showLoading({
+        title: '搜索中...',
+        icon: 'Loading'
+      });
+
+      wx.request({
+        url: 'http://www.triple2.xyz:8081/schedule/major',
+        data: {
+          major: formData
+        },
+        success: function (res) {
+          console.log(res.data);
+          if (res.data.length == 0) {
+            console.log("无结果")
+            that.setData({
+              reshow: 2
+            })
+          } else {
+            console.log("有结果")
+            that.getcourseTime(res.data)
+            that.setData({
+              reshow: 1
+            })
+            console.log("reshow:")
+            console.log(that.data.reshow)
+          }
+          wx.hideLoading();
+        }
+      })
+    }
+  },
+  // 按老师搜索
+  teacher_formSubmit: function (e) {
+    let that = this;
+    let formData = e.detail.value.keyword;// 关键词
+    console.log(formData);
+
+    if (formData == '') {
+      wx.showToast({
+        title: '输入不能为空',
+        icon: 'none'
+      })
+    } else {
+      wx.showLoading({
+        title: '搜索中...',
+        icon: 'Loading'
+      });
+
+      wx.request({
+        url: 'http://www.triple2.xyz:8081/schedule/teacher',
+        data: {
+          tname: formData
+        },
+        success: function (res) {
+          console.log(res.data);
+          if (res.data.length == 0) {
+            console.log("无结果")
+            that.setData({
+              reshow: 2
+            })
+          } else {
+            console.log("有结果")
+            that.getcourseTime(res.data)
+            that.setData({
+              reshow: 1
+            })
+            console.log("reshow:")
+            console.log(that.data.reshow)
+          }
+          wx.hideLoading();
+        }
+      })
+    }
+  },
+  // 点击改变标签页
+  changetab: function (e) {
+    this.setData({
+      currentTab: e.currentTarget.dataset.id
+    })
+  },
+  // 滑动改变标签页
+  switchTab: function (e) {
+    this.setData({
+      currentTab: e.detail.current
+    });
+  },
+  
+  /*发送到resultss并改变time_detail和site_detail*/
+  getcourseTime: function (resdata) {
+    let cpy_re = resdata && resdata.map(item => {
+      item.class_time_detail = item.class_time_detail && item.class_time_detail.split(';')
+      item.site_name_detail = item.site_name_detail && item.site_name_detail.split(';')
+      item.major === null ? (item.major='其他') : null
+      return item
+    })
+    this.setData({
+      results: cpy_re
+    })
+  },
+  // 下拉刷新不知道做好了没呢
+  scroll(event) {
+    console.log(event)
+  },
+  reactBottom() {
+    console.log('触底-加载更多')
+  },
+  
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that = this;
 
+    // 获取系统信息
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          winWidth: res.windowWidth,
+          winHeight: res.windowHeight
+        });
+      }
+    });
+    console.log(that.data.winHeight)
+    console.log(that.data.winWidth)
+    wx.stopPullDownRefresh() //刷新完成后停止下拉刷新动效
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    let that = this;
+    let tbodyHeight = app.globalData.windowHeight;
+    that.setData({
+      tbodyHeight: tbodyHeight.toFixed(0)
+    })
   },
 
   /**
@@ -47,7 +228,11 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    var that = this;
+    that.setData({
+      currentTab: 0 //当前页的一些初始数据，视业务需求而定
+    })
+    this.onLoad(); //重新加载onLoad()
   },
 
   /**
@@ -63,4 +248,4 @@ Page({
   onShareAppMessage: function () {
 
   }
-})
+});
